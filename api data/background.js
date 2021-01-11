f8dbdd11b08de792c00bc31fe208106e05c4ebf5
bhupendrabanothe <bhupendrabanothe@gmail.com>
2021-01-11 20:38:50 +0530

    fix: added api part

---
+++ api data/background.js
@@ -0,0 +1,3 @@
+chrome.browserAction.onClicked.addListener(function(activeTab) {
+    chrome.tabs.create({ 'url': "file:///Users/bhupendrabanothe/Soal%20Workplace/milestone/MS_Chrome-extension_Grus_Tep_Hyderabad_Full-time_2020_2/index.html" });
+});
